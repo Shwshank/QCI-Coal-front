4ddd8bd4038b5932de2e1b2906a494dc33871893
Shubhanshu Soni <shubhanshu.soni@qcin.org>
2018-02-05 15:35:03 +0530

    Update dockerbuild.sh

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -12,15 +12,15 @@ REPONAME="coal-frontend-pipeline-V2"
 
 # git clone $GITHUB_LINK
 
-# mv /var/lib/jenkins/workspace/coal-frontend-V2/dist/ /home/ubuntu/dist_backups/coal-V2/dist_$TIMESTAMP
+mv /var/lib/jenkins/workspace/coal-frontend-V2/dist/ /home/ubuntu/dist_backups/coal-V2/dist_$TIMESTAMP
 
-# mv /var/lib/jenkins/workspace/coal-frontend-pipeline-V2/dist /var/lib/jenkins/workspace/coal-frontend-V2/
+mv /var/lib/jenkins/workspace/coal-frontend-pipeline-V2/dist /var/lib/jenkins/workspace/coal-frontend-V2/
 
-# docker stop $DOCKERNAME
+docker stop $DOCKERNAME
 
-# docker rm $DOCKERNAME
+docker rm $DOCKERNAME
 
-# docker rmi $IMAGENAME
+docker rmi $IMAGENAME
 
 docker build -t $IMAGENAME /var/lib/jenkins/workspace/coal-frontend-V2/.
 
